13b8f0a24c247f5e00acc9c95bd99aa75f8bcc71
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2016-11-21 11:22:39 -0700

    add the read subcommand

--- party/guests.js
+++ party/guests.js
@@ -1,12 +1,21 @@
 'use strict';
 var fs = require('fs');
 var path = require('path');
-var guestPath = path.join(__dirname, 'guests.json');
+var guestsPath = path.join(__dirname, 'guests.json');
 
-fs.readFile(guestPath, 'utf8', function(err, data) {
-  if (err) {
-    throw err;
-  }
-  var guests = JSON.parse(data);
-  console.log(guests);
-});
+var node = path.basename(process.argv[0]);
+var file = path.basename(process.argv[1]);
+var cmd = process.argv[2];
+
+if (cmd === 'read') {
+  fs.readFile(guestsPath, 'utf8', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    var guests = JSON.parse(data);
+    console.log(guests);
+  });
+} else {
+  console.error('Usage: ${node} ${file} read');
+  process.exit(1);
+}
